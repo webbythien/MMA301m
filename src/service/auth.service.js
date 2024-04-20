a74b3b6de8cc4885e4525bf7013509699c166b37
webbythien <thiengk563@gmail.com>
2024-04-20 09:32:35 +0700

    rm timeout

--- src/service/auth.service.js
+++ src/service/auth.service.js
@@ -2,16 +2,20 @@ require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const httpStatus = require("http-status");
 const userService = require("./user.service");
-const ApiError = require('../utils/ApiError');
+const ApiError = require("../utils/ApiError");
 
 class AuthService {
-    static loginUserWithEmailAndPassword = async (email, password) => {
-        const user = await userService.getUserByEmail(email);
-        if (!user || !(await user.isPasswordMatch(password))) {
-          throw new ApiError(httpStatus.UNAUTHORIZED, 'Incorrect email or password');
-        }
-        return user;
+  static loginUserWithEmailAndPassword = async (email, password) => {
+    const user = await userService.getUserByEmail(email);
+    if (!user || !(await user.isPasswordMatch(password))) {
+      // throw new ApiError(httpStatus.UNAUTHORIZED, 'Incorrect email or password');
+      return {
+        status: "Incorrect email or password",
+        statusCode: httpStatus.BAD_REQUEST,
       };
+    }
+    return user;
+  };
 }
 
 module.exports = AuthService;
